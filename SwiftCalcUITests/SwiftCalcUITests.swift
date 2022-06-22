6d66c3e08318b9af0697c00c7411a655660fe011
Uriel Enrique <urielenrique99@gmail.com>
2022-06-21 21:01:58 -0500

    testing sum operation

--- SwiftCalcUITests/SwiftCalcUITests.swift
+++ SwiftCalcUITests/SwiftCalcUITests.swift
@@ -56,6 +56,28 @@ class SwiftCalcUITests: XCTestCase {
         
         XCTAssert(displayText == "0")
     }
+    
+    func testAddingTwoDigits() {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let threeButton = app.buttons["3"]
+        threeButton.tap()
+        
+        let addButton = app.buttons["+"]
+        addButton.tap()
+        
+        let fiveButton = app.buttons["5"]
+        fiveButton.tap()
+        
+        let equalButton = app.buttons["="]
+        equalButton.tap()
+        
+        let display = app.staticTexts["display"]
+        let displayText = display.label
+        
+        XCTAssertEqual(displayText, "8.0")
+    }
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
